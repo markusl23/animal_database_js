185cbf07580a62e2c833c5af53faef084209a377
markusl23 <markus4github@riseup.net>
2025-10-14 17:12:03 +0200

    added animal database

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+let animalList = [];
+
+let animalList = let animalList = [
+  {
+    'name': "Orca",
+    'element': "Water",
+    'averageWeightKilogram': 4500,
+    'foodType': "Carnivore"
+  },
+  {
+    'name': "Great White Shark",
+    'element': "Water",
+    'averageWeightKilogram': 800,
+    'foodType': "Carnivore"
+  },
+  {
+    'name': "Green Sea Turtle",
+    'element': "Water",
+    'averageWeightKilogram': 150,
+    'foodType': "Herbivore"
+  },
+  {
+    'name': "Raven",
+    'element': "Air",
+    'averageWeightKilogram': 1,
+    'foodType': "Omnivore"
+  },
+  {
+    'name': "Condor",
+    'element': "Air",
+    'averageWeightKilogram': 12,
+    'foodType': "Carnivore"
+  },
+  {
+    'name': "Albatross",
+    'element': "Water",
+    'averageWeightKilogram': 7,
+    'foodType': "Herbivore"
+  },
+  {
+    'name': "Lion",
+    'element': "Land",
+    'averageWeightKilogram': 190,
+    'foodType': "Carnivore"
+  },
+  {
+    'name': "Tiger",
+    'element': "Land",
+    'averageWeightKilogram': 220,
+    'foodType': "Carnivore"
+  },
+  {
+    'name': "Wolf",
+    'element': "Land",
+    'averageWeightKilogram': 40,
+    'foodType': "Carnivore"
+  },
+  {
+    'name': "Elephant",
+    'element': "Land",
+    'averageWeightKilogram': 4000,
+    'foodType': "Herbivore"
+  },
+  {
+    'name': "Black Bear",
+    'element': "Land",
+    'averageWeightKilogram': 110,
+    'foodType': "Omnivore"
+  },
+];
