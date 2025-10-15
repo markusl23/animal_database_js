0eda3605ac53cb5771ccab78c641bbe24b04157f
markusl23 <markus4github@riseup.net>
2025-10-15 15:45:22 +0200

    added JS code to add animal numbers to DOM tree

--- js/scripts.js
+++ js/scripts.js
@@ -86,6 +86,9 @@ for (let i = 0; i < animalList.length; i++) {
     waterAnimalNumber++;
   }
 }
+document.querySelector('#landAnimalNumber').innerText = landAnimalNumber;
+document.querySelector('#waterAnimalNumber').innerText = waterAnimalNumber;
+document.querySelector('#airAnimalNumber').innerText = airdAnimalNumber;
 
 // listen to animal selection by user
 document.querySelector('button').addEventListener('click', () => {
