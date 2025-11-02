15727424d1bc47babbad1a684c2eb4087b2e7b4a
markusl23 <markus4github@riseup.net>
2025-11-02 13:25:22 +0100

    enclosed animalList in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,93 @@
-let animalList = [
-  {
-    'name': "Orca",
-    'element': "Water",
-    'averageWeightKilogram': 4500,
-    'foodType': "Carnivore"
-  },
-  {
-    'name': "Great White Shark",
-    'element': "Water",
-    'averageWeightKilogram': 800,
-    'foodType': "Carnivore"
-  },
-  {
-    'name': "Green Sea Turtle",
-    'element': "Water",
-    'averageWeightKilogram': 150,
-    'foodType': "Herbivore"
-  },
-  {
-    'name': "Raven",
-    'element': "Air",
-    'averageWeightKilogram': 1,
-    'foodType': "Omnivore"
-  },
-  {
-    'name': "Condor",
-    'element': "Air",
-    'averageWeightKilogram': 12,
-    'foodType': "Carnivore"
-  },
-  {
-    'name': "Albatross",
-    'element': "Air",
-    'averageWeightKilogram': 7,
-    'foodType': "Herbivore"
-  },
-  {
-    'name': "Lion",
-    'element': "Land",
-    'averageWeightKilogram': 190,
-    'foodType': "Carnivore"
-  },
-  {
-    'name': "Tiger",
-    'element': "Land",
-    'averageWeightKilogram': 220,
-    'foodType': "Carnivore"
-  },
-  {
-    'name': "Wolf",
-    'element': "Land",
-    'averageWeightKilogram': 40,
-    'foodType': "Carnivore"
-  },
-  {
-    'name': "Elephant",
-    'element': "Land",
-    'averageWeightKilogram': 4000,
-    'foodType': "Herbivore"
-  },
-  {
-    'name': "Black Bear",
-    'element': "Land",
-    'averageWeightKilogram': 110,
-    'foodType': "Omnivore"
-  },
-];
+let animalRepository = (function () {
+
+  let animalList = [
+    {
+      'name': "Orca",
+      'element': "Water",
+      'averageWeightKilogram': 4500,
+      'foodType': "Carnivore"
+    },
+    {
+      'name': "Great White Shark",
+      'element': "Water",
+      'averageWeightKilogram': 800,
+      'foodType': "Carnivore"
+    },
+    {
+      'name': "Green Sea Turtle",
+      'element': "Water",
+      'averageWeightKilogram': 150,
+      'foodType': "Herbivore"
+    },
+    {
+      'name': "Raven",
+      'element': "Air",
+      'averageWeightKilogram': 1,
+      'foodType': "Omnivore"
+    },
+    {
+      'name': "Condor",
+      'element': "Air",
+      'averageWeightKilogram': 12,
+      'foodType': "Carnivore"
+    },
+    {
+      'name': "Albatross",
+      'element': "Air",
+      'averageWeightKilogram': 7,
+      'foodType': "Herbivore"
+    },
+    {
+      'name': "Lion",
+      'element': "Land",
+      'averageWeightKilogram': 190,
+      'foodType': "Carnivore"
+    },
+    {
+      'name': "Tiger",
+      'element': "Land",
+      'averageWeightKilogram': 220,
+      'foodType': "Carnivore"
+    },
+    {
+      'name': "Wolf",
+      'element': "Land",
+      'averageWeightKilogram': 40,
+      'foodType': "Carnivore"
+    },
+    {
+      'name': "Elephant",
+      'element': "Land",
+      'averageWeightKilogram': 4000,
+      'foodType': "Herbivore"
+    },
+    {
+      'name': "Black Bear",
+      'element': "Land",
+      'averageWeightKilogram': 110,
+      'foodType': "Omnivore"
+    },
+  ];
+
+  function getAll () {
+    return animalList;
+  }
+
+  function add (newAnimal) {
+    animalList.push(newAnimal);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  }
+
+})()
+
+let currentAnimalList = animalRepository.getAll();
+
+// console.log(currentAnimalList);
+// console.log(animalRepository.getAll());
 
 // animal attributes distribution
 
@@ -73,14 +95,14 @@ let animalList = [
 let landAnimalNumber = 0;
 let waterAnimalNumber = 0;
 let airAnimalNumber = 0;
-for (let i = 0; i < animalList.length; i++) {
-  if (animalList[i].element === 'Land') {
+for (let i = 0; i < currentAnimalList.length; i++) {
+  if (currentAnimalList[i].element === 'Land') {
     landAnimalNumber++;
   }
-  else if (animalList[i].element === 'Water') {
+  else if (currentAnimalList[i].element === 'Water') {
     waterAnimalNumber++;
   }
-  else if (animalList[i].element === 'Air') {
+  else if (currentAnimalList[i].element === 'Air') {
     airAnimalNumber++;
   }
 }
@@ -91,20 +113,20 @@ document.querySelector('#waterAnimalNumber').innerText = waterAnimalNumber;
 document.querySelector('#airAnimalNumber').innerText = airAnimalNumber;
 
 // find highest & lowest animal weight, and corresponding animal name
-let maxAnimalWeight = (animalList[0].averageWeightKilogram + animalList[1].averageWeightKilogram + 0.1) / 2;
+let maxAnimalWeight = (currentAnimalList[0].averageWeightKilogram + currentAnimalList[1].averageWeightKilogram + 0.1) / 2;
 let maxWeightAnimalName = '';
-let minAnimalWeight = (animalList[0].averageWeightKilogram + animalList[1].averageWeightKilogram + 0.1) / 2;
+let minAnimalWeight = (currentAnimalList[0].averageWeightKilogram + currentAnimalList[1].averageWeightKilogram + 0.1) / 2;
 let minWeightAnimalName = '';
 
-// loop over animalList, compare weight numbers & find min/max + name
-for (let i = 0; i < animalList.length; i++) {
-  if (animalList[i].averageWeightKilogram > maxAnimalWeight) {
-    maxAnimalWeight = animalList[i].averageWeightKilogram;
-    maxWeightAnimalName = animalList[i].name;
+// loop over currentAnimalList, compare weight numbers & find min/max + name
+for (let i = 0; i < currentAnimalList.length; i++) {
+  if (currentAnimalList[i].averageWeightKilogram > maxAnimalWeight) {
+    maxAnimalWeight = currentAnimalList[i].averageWeightKilogram;
+    maxWeightAnimalName = currentAnimalList[i].name;
   }
-  else if (animalList[i].averageWeightKilogram < minAnimalWeight) {
-  minAnimalWeight = animalList[i].averageWeightKilogram;
-  minWeightAnimalName = animalList[i].name;
+  else if (currentAnimalList[i].averageWeightKilogram < minAnimalWeight) {
+  minAnimalWeight = currentAnimalList[i].averageWeightKilogram;
+  minWeightAnimalName = currentAnimalList[i].name;
   }
 }
 
@@ -118,14 +140,14 @@ document.querySelector('#minAnimalWeight').innerText = minAnimalWeight;
 let carnivoreAnimalNumber = 0;
 let herbivoreAnimalNumber = 0;
 let omnivoreAnimalNumber = 0;
-for (let i = 0; i < animalList.length; i++) {
-  if (animalList[i].foodType === 'Carnivore') {
+for (let i = 0; i < currentAnimalList.length; i++) {
+  if (currentAnimalList[i].foodType === 'Carnivore') {
     carnivoreAnimalNumber++;
   }
-  else if (animalList[i].foodType === 'Herbivore') {
+  else if (currentAnimalList[i].foodType === 'Herbivore') {
     herbivoreAnimalNumber++;
   }
-  else if (animalList[i].foodType === 'Omnivore') {
+  else if (currentAnimalList[i].foodType === 'Omnivore') {
     omnivoreAnimalNumber++;
   }
 }
@@ -141,16 +163,16 @@ document.querySelector('button').addEventListener('click', () => {
   let currentAnimal = document.querySelector('#animalName').value;
   // add selected animal name to DOM tree
   document.querySelector('#animalName_key').innerText = currentAnimal;
-  // find animal element/habitat in array "animalList"
-  currentElement = animalList.find(a => a.name === currentAnimal)?.element ?? null;
+  // find animal element/habitat in array "currentAnimalList"
+  currentElement = currentAnimalList.find(a => a.name === currentAnimal)?.element ?? null;
   // add animal element/habitat to DOM tree
   document.querySelector('#element').innerText = currentElement;
-  // find animal weight in array "animalList"
-  currentWeight = animalList.find(a => a.name === currentAnimal)?.averageWeightKilogram ?? null;
+  // find animal weight in array "currentAnimalList"
+  currentWeight = currentAnimalList.find(a => a.name === currentAnimal)?.averageWeightKilogram ?? null;
   // add animal weight to DOM tree
   document.querySelector('#weight').innerText = currentWeight;
-  // find animal food in array "animalList"
-  currentFood = animalList.find(a => a.name === currentAnimal)?.foodType ?? null;
+  // find animal food in array "currentAnimalList"
+  currentFood = currentAnimalList.find(a => a.name === currentAnimal)?.foodType ?? null;
   // add animal food to DOM tree
   document.querySelector('#food').innerText = currentFood;
 });
